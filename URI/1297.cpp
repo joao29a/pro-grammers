d75ee305974ebd9cc40d52662975bbfc099d7326
John <john@john-programmer.(none)>
2013-09-13 17:10:58 -0300

    1297

---
+++ URI/1297.cpp
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <cstring>
+#include <float.h>
+#include <cmath>
+
+#define BUF_SIZE 10240
+char buffer[BUF_SIZE];
+
+double doFunction(int** p,int** q, int k,double x){
+    double pRes =0;
+    double qRes =0;
+    for(int i = 0; i < k; i++){
+        pRes += (*p[i])*pow(x,i);
+        printf("%d\n%f\n",p[i],pRes);
+    }
+    for(int i = 0; i < k; i++){
+        qRes += (*q[i])*pow(x,i);
+    }
+    if(qRes == 0)
+        return DBL_MAX;
+    else
+        return (pRes/qRes);
+}
+
+void loopLer(int k, int** vet){
+    for(int i = 0; i < k; i++){
+        sscanf(buffer,"%d %[^\n]",&(*vet[i]),buffer);
+    }
+}
+
+int main(){
+    int cont = 1;
+    int w,d,a,k;
+    int *p1,*p2;
+    int *q1,*q2;
+    while (fgets(buffer,BUF_SIZE,stdin) != NULL){
+        if (cont == 1){
+            sscanf(buffer,"%d %d %d %d",&w,&d,&a,&k);
+            p1 = (int*)malloc(sizeof(int)*(k+1));
+            q1 = (int*)malloc(sizeof(int)*(k+1));
+            p2 = (int*)malloc(sizeof(int)*(k+1));
+            q2 = (int*)malloc(sizeof(int)*(k+1));
+            k += 1;
+        }
+        else if (cont == 2){
+            loopLer(k,&p1);
+        }
+        else if (cont == 3){
+            loopLer(k,&q1);
+        }
+        else if (cont == 4){
+            loopLer(k,&p2);
+        }
+        else if (cont == 5){
+            loopLer(k,&q2);
+            cont = 0;
+            printf("%f\n",doFunction(&p1,&q1,k,2));
+            free(p1);
+            free(p2);
+            free(q1);
+            free(q2);
+        }
+        buffer[0]='\0';
+        cont++;
+    }
+    return 0;
+}
